b099d34eddc2d0ff7ac61839aa626aff37ea0be0
Titus Parsitau <54472156+Tito94-hub@users.noreply.github.com>
2022-06-11 18:03:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+
+
+# game = [Rock_Paper_Scissors]
+
+import random
+import choice
+
+"R" for "rock", 
+"P" for "paper", 
+"S" for "scissors"
+
+Rock_Paper_Scissors = input
+
+Users = CPU vs Player
+
+User = to pick "R", "P" or "S"
+
+Option = ["R", "P","S"]
+print(User to pick an option between "R","P" or "S")
+   
+   if(user input is invalid):
+       print('error')
+   else:
+       print(ask user for input again) 
+
+print(random.choice(CPU))
+
+player moves =  []
+
+(Player)Rock : Scissors(CPU)
+(Player)Scissors : Paper(CPU)
+(Player)Paper : Rock(CPU)
+
+Winner = random.choice for player moves in (Option):
+     print(Winner)
+
+    if its a tie (Player and CPU pick the same move) restart the game in + print(random.choice(CPU))
